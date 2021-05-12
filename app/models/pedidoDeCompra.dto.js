49a775f97c8505ceac9e072208e1c98ecf9f290f
Kaio <kaio.moraes@zup.com.br>
2021-05-12 14:49:01 -0300

    Criar classe pedidoDeCompra.dto.

---
+++ app/models/pedidoDeCompra.dto.js
@@ -0,0 +1,12 @@
+export default class PedidoDeCompra {
+
+constructor(dataDeVencimento , saldo , dataDePagamento , emailResponsavel , dataLimiteEnvio , formaDePagamento , cnpjOuCpf)
+{
+this.dataDeVencimento = dataDeVencimento;
+this.saldo = saldo;
+this.dataDePagamento = dataDePagamento;
+this.emailResponsavel = emailResponsavel;
+this.dataLimiteEnvio = dataLimiteEnvio;
+this.formaDePagamento = formaDePagamento;
+this.cnpjOuCpf = cnpjOuCpf; }
+};
