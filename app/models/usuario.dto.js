c3ed50f3a46ac0dbad10d2234990e504e65943b1
Kaio <kaio.moraes@zup.com.br>
2021-05-12 12:55:06 -0300

    Criar classe usuario.dto.

---
+++ app/models/usuario.dto.js
@@ -0,0 +1,9 @@
+export default class Usuario {
+
+constructor(nomeCompleto , email , senha , nivelDeAcesso)
+{
+this.nomeCompleto = nomeCompleto;
+this.email = email;
+this.senha = senha;
+this.nivelDeAcesso = nivelDeAcesso; }
+};
