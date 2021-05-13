f89ac222e7823b4d1674d0845f0fda32e82b21fc
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-13 13:40:49 -0300

    Criado regra de negocio para cadastrar responsavel

--- app/component/responsavel/responsavel.component.js
+++ app/component/responsavel/responsavel.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../header';
 import Footer from '../footer';
-import {Container, Form, Row, Col, InputGroup, Button } from 'react-bootstrap';
+import {Container, Form, Row, Col, InputGroup, Button, Modal } from 'react-bootstrap';
 import { AiOutlineMail } from 'react-icons/ai';
 import  ResponsavelDTO  from '../../models/responsavel.dto';
 import  ResponsavelService  from '../../service/responsavel.service';
@@ -10,10 +10,14 @@ export default class FormularioResponsavel extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {nomeCompleto: '', email: '', nomeDoProjeto: ''};
+        this.state = {nomeCompleto: '', email: '', nomeDoProjeto: '', modalHide: false,showModalSucesso: false, showModalError: false, showModalAcesso: false};
         this.service = new ResponsavelService();
+
         this.handlerChange = this.handlerChange.bind(this);
         this.handlerSubmit = this.handlerSubmit.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.handleCloseModalError = this.handleCloseModalError.bind(this);
+        this.handleCloseModalAcesso = this.handleCloseModalAcesso.bind(this);
     }
 
     handlerChange(event) {
@@ -30,7 +34,37 @@ export default class FormularioResponsavel extends React.Component {
         const responsavel = new ResponsavelDTO(this.state.nomeCompleto, this.state.email, this.state.nomeDoProjeto);
         const resposta    = this.service.cadastrarNovoResponsavel(responsavel);
         event.preventDefault();
+
+        resposta.then((response) => {
+            if (response.status == 201) {
+                // responsável cadastro com sucesso
+                this.setState({showModalSucesso: true});
+            }
+        }).catch((error) => {
+            if (error.response.status == 403) {
+                // acesso negado - usuário não está logado ou não tem permissão
+                this.setState({showModalAcesso: true});
+            } else if (error.response.status == 400) {
+                // erro de dados
+                this.setState({showModalError: true});
+            }
+        });
+
     }
+
+    
+    handleCloseModal(event) {
+        this.setState({showModalSucesso: false});
+    }
+
+    handleCloseModalError(event) {
+        this.setState({showModalError: false});
+    }
+
+    handleCloseModalAcesso() {
+        this.setState({showModalAcesso: false});
+    }
+
 
     render() {
         return (
@@ -95,7 +129,53 @@ export default class FormularioResponsavel extends React.Component {
                 </Row>
             </Form>
             </Container>
-                <Footer />
+            <Footer />
+
+            <div>
+                <Modal id="modalSucesso" name="modalScuesso" show={this.state.showModalSucesso}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Cadastro de responsável</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Cadastro realizado com sucesso!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModal}>
+                            Fechar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+            <div>
+                <Modal id="modalError" name="modalError" show={this.state.showModalError}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Cadastro de responsável</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Existem erros de dados, favor verificar!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalError}>
+                            Fechar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+            <div>
+                <Modal id="modalAccessoNegado" name="modalAccessoNegado" show={this.state.showModalAcesso}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Cadastro de responsável</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Usuário não tem permissão para cadastrar um responsável.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalAcesso}>
+                            Fechar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
             </Container>
         );
     }
