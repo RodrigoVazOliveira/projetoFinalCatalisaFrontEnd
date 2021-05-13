f8c8ea804779125642dbec2cd08f9fdeb8980b0a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-13 01:45:41 -0300

    ajustar serviço de usuario

--- app/service/usuario.service.js
+++ app/service/usuario.service.js
@@ -1,20 +1,17 @@
 import axios from "axios";
-import Login from '../models/usuario.dto';
+export default class UsuarioService{
+    cadastrarNovoUsuario(usuarioDto) {
+        axios.post('http://localhost:8080/usuarios/', JSON.stringify(usuarioDto), {
+            headers: {
+                'Content-Type':'application/json',
 
-export default class UsuarioService{autenticar(nomeCompleto, email, senha, nivelDeAcesso) {
-                                    let usuarioDto = new Usuario(nomeCompleto, email, senha, nivelDeAcesso)
-                                    axios.post('http://localhost:8080/usuario', JSON.stringify(usuarioDto), {
-                                                          headers: {
-                                                              'Content-Type':'application/json',
-                                                              'Access-Control-Allow-Origin': '*',
-                                                              'Access-Control-Allow-Methods': 'POST, PUT, DELETE, OPTIONS, GET'
-                                                          }
-                                                      }).then(
-                                                          res => {
-                                                              console.log(res.headers);
-                                                          }
-                                                      ).catch(function(error) {
-                                                          console.log(error);
-                                                      });
-                                                  }
-                                              };
+            }
+        }).then(
+            res => {
+                console.log(res.headers);
+            }
+        ).catch(function(error) {
+            console.log(error);
+        });
+    }
+};
