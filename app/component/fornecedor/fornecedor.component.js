702d0b314deff245b048b24012002089f1472216
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-13 23:13:24 -0300

    bind das funções do fornecedor

--- app/component/fornecedor/fornecedor.component.js
+++ app/component/fornecedor/fornecedor.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Form, Button, Container, Row, Col, InputGroup, Nav, Modal } from 'react-bootstrap';
+import Input from '../helper/input';
 import Header from '../header';
 import Footer from '../footer';
 
@@ -20,12 +21,39 @@ export default class FormCadastroFornecedor extends React.Component {
             email: '',
             categoriaDeCusto: ''
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value  = target.value;
+        const name   = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
         return (
             <Container>
                 <Header nomePagina="Cadastro de fornecedor" />
+                <Form onSubmit={handleSubmit}>
+                    <Input 
+                        label="CPF"
+                        tipo="text"
+                        handler={this.handleChange}
+                        value={this.state.cpf}
+                        name="cpf"
+                        idInput="cpf"
+                        index="1" />
+                </Form>
                 <Footer />
             </Container>
         );
